2862daff0bc8fed8e7928b382da7e31fd49fee4d
Martin Lund <the_tribe2003@hotmail.com>
2018-02-22 13:04:07 +0100

    Added a more simple example for better understanding

---
+++ Clone/DeepClone/DeepClone/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepClone
+{
+    class Address
+    {
+        public string Street { get; set; }
+        public int ZIP { get; set; }
+        public string City { get; set; }
+
+        public Address(string street, int zip, string city)
+        {
+            this.Street = street;
+            this.ZIP = zip;
+            this.City = city;
+        }
+
+        public Address DeepClone()
+        {
+            return new Address(Street, ZIP, City);
+        }
+
+    }
+}
---
+++ Clone/DeepClone/DeepClone/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepClone
+{
+    class Person
+    {
+
+        public Address Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Weight { get; set; }
+
+        public Person(Address address, DateTime dateOfBirth, double weight)
+        {
+            this.Address = address;
+            this.DateOfBirth = dateOfBirth;
+            this.Weight = weight;
+        }
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person other = ShallowCopy();
+            other.Address = Address.DeepClone();
+            return other;
+        }
+    }
+}
