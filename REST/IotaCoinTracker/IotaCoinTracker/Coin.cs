ca6672fd9bb4ab71e04607c33d9dd7c506e92101
Martin Lund <the_tribe2003@hotmail.com>
2018-02-27 11:51:30 +0100

    RestSharp implementation --> Json deserialization to come

---
+++ REST/IotaCoinTracker/IotaCoinTracker/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IotaCoinTracker
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public int Rank { get; set; }
+        public double Price_USD { get; set; }
+    }
+}
---
+++ REST/IotaCoinTracker/IotaCoinTracker/Program.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace IotaCoinTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Please make a selection");
+            Console.WriteLine("1. Bitcoin");
+            Console.WriteLine("2. Ethereum");
+            Console.WriteLine("3. Iota");
+            Console.WriteLine("4. Litecoin");
+            int selection = int.Parse(Console.ReadLine());
+            RestClient client = new RestClient("https://api.coinmarketcap.com/v1/ticker");
+            RestRequest request = new RestRequest(Method.GET);
+            string coinType = "";
+            switch (selection)
+            {
+                case 1:
+                    coinType = "bitcoin";
+                    break;
+                case 2:
+                    coinType = "ethereum";
+                    break;
+                case 3:
+                    coinType = "iota";
+                    break;
+                case 4:
+                    coinType = "litecoin";
+                    break;
+                default:
+                    coinType = "";
+                    break;
+            }
+
+            request.Resource = "/{coinType}/";
+            request.AddParameter("coinType", coinType, ParameterType.UrlSegment);
+            var response = client.Execute(request);
+            string json = response.Content;
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
