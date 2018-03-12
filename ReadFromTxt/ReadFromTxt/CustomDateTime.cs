58ef74dcaa13a9641cd04b560497f13560299850
Martin Lund <the_tribe2003@hotmail.com>
2018-03-12 16:06:43 +0100

    Text file = Bikes passing by a specific place in Odense - Reading from that an creating datetime objects from it

---
+++ ReadFromTxt/ReadFromTxt/CustomDateTime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ReadFromTxt
+{
+    public class Datatime
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Hour { get; set; }
+        public string Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Datatime(string input)
+        {
+            DateTime dDate;
+
+            if (DateTime.TryParse(input, out dDate))
+            {
+                DateTime inputDate = DateTime.Parse(input);
+                Year = inputDate.Year;
+                Month = inputDate.Month;
+                Day = inputDate.Day;
+                Hour = inputDate.Hour;
+                Minutes = inputDate.Minute.ToString("D2");
+                Seconds = inputDate.Second;
+            }
+            else
+            {
+                Console.WriteLine("Invalid date");
+            }             
+        }
+
+        public override string ToString()
+        {
+            return Year + "-" + Month + "-" + Day + " " + Hour + ":" + Minutes + ":" + Seconds;
+        }
+    }
+}
---
+++ ReadFromTxt/ReadFromTxt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadFromTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\the_t\Desktop\Projekter\LearningCSharp\ReadFromTxt\ReadFromTxt\data.txt");
+            List<CustomDateTime> dates = new List<CustomDateTime>();
+            foreach (string line in lines)
+            {
+                CustomDateTime datatime = new CustomDateTime(line);
+                dates.Add(datatime);
+            }
+            foreach (CustomDateTime date in dates)
+            {
+                Console.WriteLine(date.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
