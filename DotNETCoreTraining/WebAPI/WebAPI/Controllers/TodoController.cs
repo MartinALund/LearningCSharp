4e9ad282cc6cbcd6db50d791f3c7c62eae05fb77
Martin Lund <the_tribe2003@hotmail.com>
2018-03-16 12:53:08 +0100

    Working on a WebApi Example - /api/Todo gets all items, /api/Todo/id gets specific id

---
+++ DotNETCoreTraining/WebAPI/WebAPI/Controllers/TodoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext todoContext;
+
+        public TodoController(TodoContext context)
+        {
+            todoContext = context;
+            if (todoContext.TodoItems.Count() == 0)
+            {
+                todoContext.TodoItems.Add(new TodoItem { Name = "Vaske tøj" });
+                todoContext.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public List<TodoItem> GetAll()
+        {
+            return todoContext.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name ="GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = todoContext.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
