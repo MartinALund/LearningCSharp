1edfb811f75966856f9f5511df9f0bb24a9e10ef
Martin Lund <the_tribe2003@hotmail.com>
2018-03-17 15:06:32 +0100

    Added rating prop to movie

--- DotNETCoreTraining/RazorPages/RazorPages/Migrations/20180317140325_Rating.Designer.cs
+++ DotNETCoreTraining/RazorPages/RazorPages/Migrations/20180317140325_Rating.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace RazorPages.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20180317140325_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
