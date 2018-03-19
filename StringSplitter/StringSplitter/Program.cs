b1e623f21c501efc6bf957f37d304f05a94ded4e
Martin Lund <the_tribe2003@hotmail.com>
2018-03-19 15:11:54 +0100

    Working on a string splitter. Purpose of this project is to input a large set of text, and get the output into <p> </p> tags separated on .

---
+++ StringSplitter/StringSplitter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace StringSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "String splitter. På punktum. Hver paragraph bliver indskudt i et <p> tag.";
+            string[] dataArray = data.Split(".");
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine(dataArray.Length);
+            foreach (var item in dataArray)
+            {
+                builder.Append("<p>");
+                builder.Append(item.Trim() + ".");
+                builder.Append("</p>");
+                builder.Append(Environment.NewLine);
+            }
+            Console.WriteLine(builder.ToString());           
+        }
+    }
+}
