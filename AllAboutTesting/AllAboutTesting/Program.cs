7a30d55fae3bd4bd2d4583a4f006c0955d4476b0
Martin Lund <the_tribe2003@hotmail.com>
2018-09-10 08:27:06 +0200

    Added base testing program for all testing done in C#

---
+++ AllAboutTesting/AllAboutTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllAboutTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
