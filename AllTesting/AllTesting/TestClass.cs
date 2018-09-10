4f8fc20aeab343158c145be839f82c81269754fd
Martin Lund <the_tribe2003@hotmail.com>
2018-09-10 10:37:50 +0200

    Added Xunit and more

---
+++ AllTesting/AllTesting/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllTesting
+{
+    public class TestClass
+    {
+
+        [Fact]
+        void PositiveTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        void NegativeTest()
+        {
+            Assert.Equal(4, Add(2, 3));
+        }
+   
+
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
